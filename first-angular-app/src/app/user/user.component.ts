b733699884112c6c5810af84fcde0942521ef6ab
guehara-code <gil_uehara@yahoo.com.br>
2024-07-10 08:50:38 -0300

    task complete button function implemented

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -1,16 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { User } from './user.model'
 
-// type User = {
-//   id: string;
-//   avatar: string;
-//   name: string;
-// }
-
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
 
 @Component({
   selector: 'app-user',
@@ -23,7 +13,8 @@ export class UserComponent {
   // @Input({ required: true }) id!: string;
   // @Input({ required: true }) avatar!: string;
   // @Input({ required: true }) name!: string;
-  @Input({required:true}) user!: User;
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
