1cbdb5597d49ac4616cf51a561e0781500f03338
guehara-code <gil_uehara@yahoo.com.br>
2024-07-16 15:45:50 -0300

    investment calculator - update to use modules

--- 01-starting-project/src/app/investment-results/investment-results.component.ts
+++ 01-starting-project/src/app/investment-results/investment-results.component.ts
@@ -1,11 +1,11 @@
-import { CurrencyPipe } from '@angular/common';
+// import { CurrencyPipe } from '@angular/common';
 import { Component, computed, inject } from '@angular/core';
 import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
-  standalone: true,
-  imports: [CurrencyPipe],
+  standalone: false,
+  // imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
--- 01-starting-project/src/app/user-input/user-input.component.ts
+++ 01-starting-project/src/app/user-input/user-input.component.ts
@@ -1,15 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
 // import { InvestmentInput } from '../investment-input.model';
 import { InvestmentService } from '../investment.service';
 // import { InvestmentInput } from '../investment-input.model';
 
 @Component({
   selector: 'app-user-input',
-  standalone: true,
-  imports: [FormsModule],
+  standalone: false,
+  // imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
   // @Output() calculate = new EventEmitter<InvestmentInput>();
