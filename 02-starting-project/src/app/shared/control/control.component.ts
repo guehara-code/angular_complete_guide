8b94dc7c26bdaf5d65371fff7939a1b5ffd1c7ff
guehara-code <gil_uehara@yahoo.com.br>
2024-07-22 08:53:41 -0300

    server status app - contentChild function test

--- 02-starting-project/src/app/shared/control/control.component.ts
+++ 02-starting-project/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostBinding, HostListener, ViewEncapsulation, inject, input } from '@angular/core';
+import { Component, ContentChild, ElementRef, HostBinding, HostListener, ViewEncapsulation, contentChild, inject, input } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -19,9 +19,12 @@ export class ControlComponent {
   // }
   label = input.required<string>();
   private el = inject(ElementRef);
+  // @ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>;
+  private control = contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input');
 
   onClick() {
     console.log('Clicked');
     console.log(this.el);
+    console.log(this.control());
   }
 }
