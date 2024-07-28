20346b62c6cdf292304d758efa5a383a9b4b8651
guehara-code <gil_uehara@yahoo.com.br>
2024-07-28 08:29:08 -0300

    new pipes example - custom pipe aplied to all values and decimal number fixed

--- 04-starting-project/src/app/temperature.pipe.ts
+++ 04-starting-project/src/app/temperature.pipe.ts
@@ -5,7 +5,14 @@ import { Pipe, PipeTransform } from "@angular/core";
     standalone: true
 })
 export class TemperaturePipe implements PipeTransform {
-    transform(value: string | number) {
+    transform(value: string | number | null,
+        inputType: 'cel' | 'fah',
+        outputType?: 'cel' | 'fah'
+    ) {
+        if (!value) {
+            return value;
+        }
+
         let val: number;
 
         if (typeof value === 'string') {
@@ -14,8 +21,23 @@ export class TemperaturePipe implements PipeTransform {
             val = value;
         }
 
-        const outputTemp = val * (9/5) + 32;
+        let outputTemp: number;
 
-        return `${outputTemp} ºF`;
+        if (inputType == 'cel' && outputType === 'fah') {
+            outputTemp = val * (9 / 5) + 32;
+        } else if (inputType === 'fah' && outputType === 'cel') {
+            outputTemp = (val - 32) * (5 / 9);
+        } else {
+            outputTemp = val;
+        }
+
+        let symbol: 'ºC' | 'ºF';
+
+        if(!outputType) {
+            symbol = inputType === 'cel' ? 'ºC' : 'ºF';
+        } else {
+            symbol = outputType === 'cel' ? 'ºC' : 'ºF';
+        }
+        return `${outputTemp.toFixed(2)} ${symbol}`;
     }
 }
