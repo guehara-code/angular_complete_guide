1cd260588f9b320db93a573afd6cf3547576bc67
guehara-code <gil_uehara@yahoo.com.br>
2024-08-03 20:44:03 -0300

    rxjs example - custem observable created

--- 07-starting-project/src/app/app.component.ts
+++ 07-starting-project/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
-import { interval, map } from 'rxjs';
+import { Component, computed, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { interval, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,15 +9,42 @@ import { interval, map } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   clickCount = signal(0);
+  clickCount$ = toObservable(this.clickCount);
+  interval$ = interval(1000);
+  intervalSignal = toSignal(this.interval$, { initialValue: 0 });
+  // interval = signal(0);
+  // doubleInterval = computed(() => this.interval() * 2);
+  customInteval$ = new Observable((subscriber) => {
+    let timesExecuted = 0;
+    const interval = setInterval(() => {
+      if (timesExecuted > 3) {
+        clearInterval(interval);
+        subscriber.complete();
+        return;
+      }
+      console.log('Emitting new value...');
+      subscriber.next({message: 'New value' });
+      timesExecuted++;
+    }, 2000);
+  });
   private destroyRef = inject(DestroyRef);
 
   constructor() {
-    effect(() => {
-      console.log(`Clicked button ${this.clickCount()} times.`)
-    })
+    // effect(() => {
+    //   console.log(`Clicked button ${this.clickCount()} times.`)
+     
+    // })
+    // effect(() => {
+      
+    //   console.log(this.doubleInterval());
+    // })
+    // toObservable(this.clickCount);
   }
 
   ngOnInit(): void {
+    // setInterval(() => {
+    //   this.interval.update(prevIntervalNumber => prevIntervalNumber + 1)
+    // }, 1000);
     // const subscription = interval(1000).pipe(
     //   map((val) => val * 2)
     // ).subscribe({
@@ -26,6 +54,17 @@ export class AppComponent implements OnInit {
     // this.destroyRef.onDestroy(() => {
     //   subscription.unsubscribe();
     // })
+    this.customInteval$.subscribe({
+      next: (val) => console.log(val),
+      complete: () => console.log('Completed!')
+    });
+    const subscription = this.clickCount$.subscribe({
+      next: (val) => console.log(`Clicked button ${val} times.`)
+    });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+
   }
 
   onClick() {
