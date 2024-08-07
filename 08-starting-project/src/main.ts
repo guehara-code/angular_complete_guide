0383733d5acfa42235e7f601f2509d02c30b1438
guehara-code <gil_uehara@yahoo.com.br>
2024-08-07 16:59:10 -0300

    place picker app - incoming interceptor included

--- 08-starting-project/src/main.ts
+++ 08-starting-project/src/main.ts
@@ -1,7 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpEventType, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 function loggingInterceptor(
     request: HttpRequest<unknown>, 
@@ -11,7 +12,17 @@ function loggingInterceptor(
     // });    
     console.log('[Outgoing Request]');
     console.log(request);
-    return next(request);
+    return next(request).pipe(
+        tap({
+            next: event => {
+                if (event.type === HttpEventType.Response) {
+                    console.log('[Incoming Response]');
+                    console.log(event.status);
+                    console.log(event.body);
+                }
+            }
+        })
+    );
 }
 
 bootstrapApplication(AppComponent, {
