22bbb2f63799effcdeedc5313aa2583f8c520167
guehara-code <gil_uehara@yahoo.com.br>
2024-08-18 08:30:29 -0300

    easy task app - redirecting user routes and separating routes files

--- 10-starting-project/src/app/app.routes.ts
+++ 10-starting-project/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from "@angular/router";
-import { TasksComponent } from "./tasks/tasks.component";
+import { routes as userRoutes } from "./users/users.routes"
+
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+
 import { NotFoundComponent } from "./not-found-component/not-found-component";
 
 
@@ -14,16 +15,7 @@ export const routes: Routes = [
     {
         path: 'users/:userId', // <your-domain>/tasks
         component: UserTasksComponent,
-        children: [
-            {
-                path: 'tasks', // <your-domain>/users/<uid>/tasks
-                component: TasksComponent
-            }, 
-            {
-                path: 'tasks/new',
-                component: NewTaskComponent
-            }
-        ]
+        children: userRoutes
     },
     {
         path: '**',
