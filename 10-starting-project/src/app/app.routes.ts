6aea074b0702e69c55a76cff87b2b0814603f4e3
guehara-code <gil_uehara@yahoo.com.br>
2024-08-18 08:19:48 -0300

    easy task app - not found route and component included

--- 10-starting-project/src/app/app.routes.ts
+++ 10-starting-project/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+import { NotFoundComponent } from "./not-found-component/not-found-component";
+
 
 export const routes: Routes = [
     {
@@ -22,5 +24,9 @@ export const routes: Routes = [
                 component: NewTaskComponent
             }
         ]
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
     }
 ]
