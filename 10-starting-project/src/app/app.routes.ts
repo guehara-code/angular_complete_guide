c314cc1187c11ded892fdd196d4addc8fadfe6f3
guehara-code <gil_uehara@yahoo.com.br>
2024-08-19 16:05:37 -0300

    easy task app - ResolveFcn to set route's title

--- 10-starting-project/src/app/app.routes.ts
+++ 10-starting-project/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from "@angular/router";
 import { routes as userRoutes } from "./users/users.routes"
 
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
-import { resolveUserName, UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+import { resolveTitle, resolveUserName, UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 
 
@@ -13,6 +13,7 @@ export const routes: Routes = [
     {
         path: '', // <your-domain>/
         component: NoTaskComponent,
+        title: 'No task selected'
     },
     {
         path: 'users/:userId', // <your-domain>/tasks
@@ -23,7 +24,8 @@ export const routes: Routes = [
         },
         resolve: {
             userName: resolveUserName
-        }
+        },
+        title: resolveTitle
     },
     {
         path: '**',
--- 10-starting-project/src/app/users/user-tasks/user-tasks.component.ts
+++ 10-starting-project/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, computed, DestroyRef, inject, input, OnInit } from '@angular/core';
 import { UsersService } from '../users.service';
-import { ActivatedRoute, ActivatedRouteSnapshot, ResolveFn, RouterLink, RouterOutlet, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRoute, ActivatedRouteSnapshot, ResolveFn, RouterLink, RouterOutlet, RouterState, RouterStateSnapshot } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
@@ -20,16 +20,21 @@ export class UserTasksComponent {
 
   // userName = computed(() => this.userService.users.find(u => u.id === this.userId())?.name);
 
-  // ngOnInit(): void {
-  //   console.log('Input Data: ' + this.message());
-  //   console.log(this.activatedRouter);
-  //   const subscription = this.activatedRouter.paramMap.subscribe({
-  //     next: (paramMap) =>
-  //       this.userName = this.userService.users.find((u) => u.id === paramMap.get('userId'))?.name || '',
-  //   });
+//   ngOnInit(): void {
+//     //   console.log('Input Data: ' + this.message());
+//     //   console.log(this.activatedRouter);
+//     //   const subscription = this.activatedRouter.paramMap.subscribe({
+//     //     next: (paramMap) =>
+//     //       this.userName = this.userService.users.find((u) => u.id === paramMap.get('userId'))?.name || '',
+//     //   });
 
-  //   this.destroyRef.onDestroy(() => subscription.unsubscribe());
-  // }
+//     //   this.destroyRef.onDestroy(() => subscription.unsubscribe());
+//     this.activatedRouter.data.subscribe({
+//       next: data => {
+//         console.log(data);
+//       }
+//     })
+//   }
 }
 
 export const resolveUserName: ResolveFn<string> = (
@@ -38,3 +43,10 @@ export const resolveUserName: ResolveFn<string> = (
   const userName = userService.users.find((u) => u.id === activatedRoute.paramMap.get('userId'))?.name || '';
   return userName;
 };
+
+export const resolveTitle: ResolveFn<string> = (
+  activatedRoute,
+  routerState
+) => {
+  return resolveUserName(activatedRoute, routerState) + '\'s Tasks'
+}
