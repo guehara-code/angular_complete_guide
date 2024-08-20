67ba1f5dc4c760a5377d8659fb083730ad7fefcf
guehara-code <gil_uehara@yahoo.com.br>
2024-08-20 08:22:09 -0300

    easy task app - lazy load TasksService

--- 10-starting-project/src/app/users/users.routes.ts
+++ 10-starting-project/src/app/users/users.routes.ts
@@ -12,46 +12,53 @@ import { TasksComponent } from '../tasks/tasks.component';
 
 
 export const resolveUserTasks: ResolveFn<Task[]> = (
-    activatedRouteSnapshot,
-    routerState
-  ) => {
-    const order = activatedRouteSnapshot.queryParams['order'];
-    const tasksService = inject(TasksService);
-    const tasks = tasksService
-      .allTasks()
-      .filter(
-        (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
-      );
-  
-    if (order && order === 'asc') {
-      tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
-    } else {
-      tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
-    }
-  
-    return tasks.length ? tasks : [];
-  };
-  
+  activatedRouteSnapshot,
+  routerState
+) => {
+  const order = activatedRouteSnapshot.queryParams['order'];
+  const tasksService = inject(TasksService);
+  const tasks = tasksService
+    .allTasks()
+    .filter(
+      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
+    );
+
+  if (order && order === 'asc') {
+    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
+  } else {
+    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
+  }
+
+  return tasks.length ? tasks : [];
+};
+
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'tasks',
-    pathMatch: 'full',
+    providers: [TasksService],
+    children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full',
 
+      },
+      {
+        path: 'tasks', // <your-domain>/users/<uid>/tasks
+        component: TasksComponent,
+        // loadComponent: () => import('../tasks/tasks.component').then(mod => mod.TasksComponent),
+        runGuardsAndResolvers: 'always',
+        resolve: {
+          userTasks: resolveUserTasks,
+        },
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+        canDeactivate: [canLeaveEditPage]
+      },
+    ]
   },
-  {
-    path: 'tasks', // <your-domain>/users/<uid>/tasks
-    component: TasksComponent,
-    // loadComponent: () => import('../tasks/tasks.component').then(mod => mod.TasksComponent),
-    runGuardsAndResolvers: 'always',
-    resolve: {
-      userTasks: resolveUserTasks,
-    },
-  },
-  {
-    path: 'tasks/new',
-    component: NewTaskComponent,
-    canDeactivate: [canLeaveEditPage]
-  },
+
 ];
